8372a3a5e578ec98a3246f5b17869f226858ef17
Phillip Cohen <phillip.cohen@10gen.com>
2012-06-17 12:00:51 -0700

    Added an icon.

--- MongoConsole/UI/MainForm.Designer.cs
+++ MongoConsole/UI/MainForm.Designer.cs
@@ -176,6 +176,7 @@
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.panel2);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "MongoConsole";
             this.tabContextMenu.ResumeLayout(false);
