dd3599c2895642a5abc412007312659feb56b574
Phillip Cohen <phillip.cohen@10gen.com>
2012-06-17 14:18:52 -0700

    Changed the default console styling to slate-on-white.

--- MongoConsole/UI/SessionPanel.Designer.cs
+++ MongoConsole/UI/SessionPanel.Designer.cs
@@ -29,42 +29,29 @@ namespace MongoConsole.UI
         /// </summary>
         private void InitializeComponent( )
         {
-            this.tbConsoleBox = new MongoConsole.UI.Component.ConsoleLogBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.cbSelectedDatabase = new System.Windows.Forms.ComboBox();
-            this.tbInput = new MongoConsole.UI.Component.HistoryTextBox();
             this.statusInsidePanel = new System.Windows.Forms.Panel();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.lblStatusHeader = new System.Windows.Forms.Label();
             this.statusPanel = new System.Windows.Forms.Panel();
+            this.tbInput = new MongoConsole.UI.Component.HistoryTextBox();
+            this.tbConsoleBox = new MongoConsole.UI.Component.ConsoleLogBox();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
             this.statusInsidePanel.SuspendLayout();
             this.statusPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tbConsoleBox
-            // 
-            this.tbConsoleBox.BackColor = System.Drawing.Color.Black;
-            this.tbConsoleBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tbConsoleBox.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbConsoleBox.ForeColor = System.Drawing.Color.GreenYellow;
-            this.tbConsoleBox.Location = new System.Drawing.Point(0, 0);
-            this.tbConsoleBox.Multiline = true;
-            this.tbConsoleBox.Name = "tbConsoleBox";
-            this.tbConsoleBox.ReadOnly = true;
-            this.tbConsoleBox.Size = new System.Drawing.Size(698, 395);
-            this.tbConsoleBox.TabIndex = 2;
-            // 
             // panel2
             // 
             this.panel2.Controls.Add(this.panel1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 367);
+            this.panel2.Location = new System.Drawing.Point(0, 361);
             this.panel2.Name = "panel2";
             this.panel2.Padding = new System.Windows.Forms.Padding(0, 4, 0, 2);
-            this.panel2.Size = new System.Drawing.Size(698, 28);
+            this.panel2.Size = new System.Drawing.Size(698, 34);
             this.panel2.TabIndex = 3;
             // 
             // panel1
@@ -72,10 +59,11 @@ namespace MongoConsole.UI
             this.panel1.Controls.Add(this.cbSelectedDatabase);
             this.panel1.Controls.Add(this.tbInput);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panel1.Location = new System.Drawing.Point(0, 4);
             this.panel1.Name = "panel1";
             this.panel1.Padding = new System.Windows.Forms.Padding(125, 0, 5, 0);
-            this.panel1.Size = new System.Drawing.Size(698, 22);
+            this.panel1.Size = new System.Drawing.Size(698, 28);
             this.panel1.TabIndex = 5;
             // 
             // cbSelectedDatabase
@@ -84,27 +72,17 @@ namespace MongoConsole.UI
             this.cbSelectedDatabase.Location = new System.Drawing.Point(6, 0);
             this.cbSelectedDatabase.Margin = new System.Windows.Forms.Padding(3, 3, 20, 3);
             this.cbSelectedDatabase.Name = "cbSelectedDatabase";
-            this.cbSelectedDatabase.Size = new System.Drawing.Size(109, 21);
+            this.cbSelectedDatabase.Size = new System.Drawing.Size(109, 25);
             this.cbSelectedDatabase.TabIndex = 1;
             this.cbSelectedDatabase.SelectedIndexChanged += new System.EventHandler(this.cbSelectedDatabase_SelectedIndexChanged);
             this.cbSelectedDatabase.Leave += new System.EventHandler(this.cbSelectedDatabase_Leave);
             // 
-            // tbInput
-            // 
-            this.tbInput.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tbInput.Location = new System.Drawing.Point(125, 0);
-            this.tbInput.MaxEntries = 1;
-            this.tbInput.Name = "tbInput";
-            this.tbInput.Size = new System.Drawing.Size(568, 21);
-            this.tbInput.TabIndex = 1;
-            // 
             // statusInsidePanel
             // 
             this.statusInsidePanel.BackColor = System.Drawing.Color.White;
-            this.statusInsidePanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.statusInsidePanel.Controls.Add(this.progressBar1);
             this.statusInsidePanel.Controls.Add(this.lblStatusHeader);
-            this.statusInsidePanel.Location = new System.Drawing.Point(96, 76);
+            this.statusInsidePanel.Location = new System.Drawing.Point(111, 54);
             this.statusInsidePanel.Name = "statusInsidePanel";
             this.statusInsidePanel.Size = new System.Drawing.Size(247, 85);
             this.statusInsidePanel.TabIndex = 4;
@@ -130,12 +108,34 @@ namespace MongoConsole.UI
             // 
             // statusPanel
             // 
-            this.statusPanel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.statusPanel.BackColor = System.Drawing.Color.White;
             this.statusPanel.Controls.Add(this.statusInsidePanel);
-            this.statusPanel.Location = new System.Drawing.Point(108, 45);
+            this.statusPanel.Location = new System.Drawing.Point(108, 69);
             this.statusPanel.Name = "statusPanel";
-            this.statusPanel.Size = new System.Drawing.Size(468, 272);
+            this.statusPanel.Size = new System.Drawing.Size(468, 192);
             this.statusPanel.TabIndex = 3;
+            // 
+            // tbInput
+            // 
+            this.tbInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbInput.Location = new System.Drawing.Point(125, 0);
+            this.tbInput.MaxEntries = 1;
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(568, 25);
+            this.tbInput.TabIndex = 1;
+            // 
+            // tbConsoleBox
+            // 
+            this.tbConsoleBox.BackColor = System.Drawing.Color.White;
+            this.tbConsoleBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbConsoleBox.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbConsoleBox.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.tbConsoleBox.Location = new System.Drawing.Point(0, 0);
+            this.tbConsoleBox.Multiline = true;
+            this.tbConsoleBox.Name = "tbConsoleBox";
+            this.tbConsoleBox.ReadOnly = true;
+            this.tbConsoleBox.Size = new System.Drawing.Size(698, 395);
+            this.tbConsoleBox.TabIndex = 2;
             // 
             // SessionPanel
             // 
